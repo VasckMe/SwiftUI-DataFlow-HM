ba996564a5b957d3c69136ea66aadddd988e22cf
VasckMe <kasarin.anton@gmail.com>
2022-10-14 13:24:47 +0300

    Initial Commit
    Project created

---
+++ SwiftUI DataFlow HM/SwiftUI DataFlow HM/SwiftUI_DataFlow_HMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DataFlow_HMApp.swift
+//  SwiftUI DataFlow HM
+//
+//  Created by Apple Macbook Pro 13 on 14.10.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DataFlow_HMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
